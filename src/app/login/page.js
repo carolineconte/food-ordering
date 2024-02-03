e37526993249a92f19baec93be43e2c952aea83c
Caroline Conte <carolinecontee@gmail.com>
2024-02-03 20:12:14 +0100

    login page

---
+++ src/app/login/page.js
@@ -0,0 +1,62 @@
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function LoginPage() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('')
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email, password
+    }
+
+
+  }
+
+
+  return (
+
+    <section className="flex-grow flex items-center">
+
+      <form className="flex flex-col gap-2 m-auto w-3/5 max-w-md px-10 py-14 rounded-3xl  bg-secondary shadow-lg"
+        onSubmit={handleFormSubmit}>
+        <h1 className="text-white/90 mb-6 uppercase text-center text-3xl">
+          Accedi</h1>
+
+        {error && (
+          <small className="text-xs text-red-500 mt-1 pl-2">{errors}</small>
+        )}
+
+        <input className="input" type="email" name="" placeholder="Email"
+          value={email} onChange={e => setEmail(e.target.value)}
+        />
+
+        <input className="input mt-4" type="password" name="" id="" placeholder="Password"
+          value={password} onChange={e => setPassword(e.target.value)}
+        />
+
+        <button className="btn block mt-4 bg-primary hover:bg-primaryHover transition">
+          Continua
+        </button>
+
+
+        <div className="mt-8 text-center text-gray-50">o accedi con provider</div>
+        <button className="btn flex gap-4 hover:bg-white/30 transition">
+          <Image src='/googleLogo.png' alt="Logo Google" width={24} height={24} />
+          Accedi con Google
+        </button>
+
+        <p className="text-gray-50 mt-8 text-center ">
+          Sei nuovo su Fiorela? <br />
+          <Link className="underline text-lg font-bold" href='/login'>Crea il tuo account &raquo;</Link>
+        </p>
+      </form>
+    </section>
+  )
+}
