05565c490471b29e84dfa7b62b31ca0f8ad785ed
Caroline Conte <carolinecontee@gmail.com>
2024-02-03 16:08:42 +0100

    att form

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Register() {
+  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
