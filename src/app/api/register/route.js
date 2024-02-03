07da30d71a88dccef73befe09b85d4cf3f7e8fef
Caroline Conte <carolinecontee@gmail.com>
2024-02-03 18:52:38 +0100

    post

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,13 @@
-export function POST(){
-  
+import mongoose from "mongoose"
+import { User } from "../../models/User"
+
+export async function POST(req){
+
+  const newUser = await req.json()
+  mongoose.connect(process.env.MONGOURL)
+
+  const userCreated = await User.create(newUser)
+
+  return Response.json(userCreated)
 }
+
